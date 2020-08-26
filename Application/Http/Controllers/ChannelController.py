25d8de94f7deb8b80f8ffd8c8a53fd598d949ce8
EvilCult <EvilCult@users.noreply.github.com>
2020-08-26 18:49:45 +0800

    添加database基本操作

--- Application/Http/Controllers/ChannelController.py
+++ Application/Http/Controllers/ChannelController.py
@@ -7,5 +7,14 @@ from Http.Models import ChannelModel
 @app.route('/channel/list')
 def channel_List():
     channelModel = ChannelModel()
-    
-    
+
+    param = {
+        'num': '1',
+        'title': 'abc',
+        'group': '央视',
+    }
+
+    result = channelModel.updateById(1, param)
+    print(result)
+
+    return 'test'
--- Application/Http/Models/ChannelModel.py
+++ Application/Http/Models/ChannelModel.py
@@ -3,20 +3,20 @@ from Http.Libs import DB
 class ChannelModel:
     def __init__(self):
         self.db = DB()
-        
-    def getListByPage(self, page):
-        pass
-        
+        self.db.tbName = 'tvg_info'
+
+    def getListByParam(self, param, typ):
+        return self.db.getListByParam(param, typ)
+
     def getById(self, id):
-        pass
-        
-    def updateById(self, id, param):
-        pass
-        
+        return self.db.getById(id)
+
+    def updateById(self, id, data):
+        return self.db.updateById(id, data)
+
     def delById(self, id):
-        pass
-       
-    def add(self, param):
-        pass
-        
-    
+        return self.db.delById(id)
+
+    def addData(self, data):
+        return self.db.addData(data)
+
