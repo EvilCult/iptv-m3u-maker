07701b3f447cc7b8b48336e8e7c9890bae84e24c
EvilCult <EvilCult@users.noreply.github.com>
2020-08-27 16:10:47 +0800

    flask加跨域支持

--- Application/Http/__init__.py
+++ Application/Http/__init__.py
@@ -2,5 +2,11 @@ from flask import Flask
 
 app = Flask('__name__', static_url_path='')
 
+def after_request(res):
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    return res
+
+app.after_request(after_request)
+
 import Http.Controllers
 import Http.Models
