2ae086b29d48af588c8d0693ad600a1ce2609a8a
EvilCult <EvilCult@users.noreply.github.com>
2023-12-06 01:10:43 +0800

    加入sqlite支持

--- Projects/Serv/Http/Controllers/Api/__init__.py
+++ Projects/Serv/Http/Controllers/Api/__init__.py
@@ -4,3 +4,6 @@ from flask import request
 
 from Http.Controllers.Api.TestController import test_blueprint
 app.register_blueprint(test_blueprint)
+
+from Http.Controllers.Api.TestXController import testx_blueprint
+app.register_blueprint(testx_blueprint)
