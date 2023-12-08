62e3d9cb72d860ec8fb991677bc41443ce81387c
EvilCult <EvilCult@users.noreply.github.com>
2023-12-09 02:38:54 +0800

    加入文本方式添加频道控件

--- Projects/Serv/Http/Controllers/Api/ChannelController.py
+++ Projects/Serv/Http/Controllers/Api/ChannelController.py
@@ -1,16 +1,11 @@
 # pyright: reportMissingModuleSource=false
 from flask import Blueprint, request
-
 from Http.Models import ChannelModel
-from Http.Libs.Middlewares import auth_middleware
-
-
 import json, time
 
 channel_blueprint = Blueprint("channel_blueprint", __name__, url_prefix="/api/v1/channel")
 
 @channel_blueprint.route('/add', methods=['POST'])
-@auth_middleware
 def api_channel_add():
     req = request.get_json()
 
@@ -26,6 +21,33 @@ def api_channel_add():
         'code': 0,
         'msg' : '',
         'data': channel_id,
+        'time': int(time.time())
+    }
+
+    return json.dumps(apiMsg)
+
+@channel_blueprint.route('/add/txt', methods=['POST'])
+def api_channel_addtxt():
+    req = request.get_json()
+
+    channel_list = []
+    channel_name = ''
+    channel_url = ''
+    for line in req['data'].splitlines():
+        if line.startswith('#EXTINF:'):
+            channel_name = line.split(',')[1]
+        elif line.startswith('http'):
+            channel_url = line
+            channel_list.append({'title': channel_name, 'url': channel_url})
+
+    channel_ids = []
+    for channel in channel_list:
+        channel_ids.append(ChannelModel().add(**channel))
+
+    apiMsg = {
+        'code': 0,
+        'msg' : '',
+        'data': channel_ids,
         'time': int(time.time())
     }
 
