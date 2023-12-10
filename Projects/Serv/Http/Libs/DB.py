d130c46f3299ef707f7de3aaad4664cbb14b7a99
EvilCult <EvilCult@users.noreply.github.com>
2023-12-10 14:14:38 +0800

    更新数据库操作方法

--- Projects/Serv/Http/Libs/DB.py
+++ Projects/Serv/Http/Libs/DB.py
@@ -5,7 +5,17 @@ class DB:
     table_name = ''
 
     def __init__(self, **kwargs):
-        self.data = kwargs
+        pass
+
+    @classmethod
+    def __init_subclass__(cls, **config):
+        super().__init_subclass__(**config)
+        cls.connect(config.get('db_name', 'Data/config.db'))
+
+    @classmethod
+    def __del_subclass__(cls):
+        cls.close()
+        super().__del_subclass__()
 
     @classmethod
     def connect(cls, db_name):
@@ -14,6 +24,94 @@ class DB:
     @classmethod
     def close(cls):
         cls.db.close()
+
+    @classmethod
+    def save(cls, **kwargs):
+        data = kwargs
+        if 'id' in data:
+            query  = f'UPDATE {cls.table_name} SET {", ".join([f"{k} = ?" for k in data.keys()])} WHERE id = ?'
+            params = list(data.values()) + [data['id']]
+        else:
+            query  = f'INSERT INTO {cls.table_name} ({", ".join(data.keys())}) VALUES ({", ".join(["?" for _ in data.values()])})'
+            params = list(data.values())
+        cls.execute(query, params)
+        if 'id' not in data:
+            return cls.execute('SELECT last_insert_rowid()')[0][0]
+
+    @classmethod
+    def remove(cls, id):
+        query = f'DELETE FROM {cls.table_name} WHERE id = ?'
+        cls.execute(query, (id,))
+
+
+    @classmethod
+    def select(cls, **kwargs):
+        cls.query = f"SELECT * FROM {cls.table_name}"
+        cls.values = []
+
+        conditions = []
+        values = []
+        for key, value in kwargs.items():
+            conditions.append(f'{key}=?')
+            values.append(value)
+        if len(conditions) > 0:
+            cls.query += f' WHERE {" AND ".join(conditions)}'
+            cls.values += values
+
+        return cls
+
+    @classmethod
+    def limit(cls, limit):
+        cls.query += f" LIMIT ?"
+        cls.values.append(limit)
+        return cls
+
+    @classmethod
+    def page(cls, page, limit):
+        cls.query += f" LIMIT ? OFFSET ?"
+        cls.values.append(limit)
+        cls.values.append((int(page) - 1) * int(limit))
+        return cls
+
+    @classmethod
+    def offset(cls, offset):
+        cls.query += f" OFFSET ?"
+        cls.values.append(offset)
+        return cls
+
+    @classmethod
+    def orderBy(cls, orderBy):
+        cls.query += f" ORDER BY {orderBy}"
+        return cls
+
+    @classmethod
+    def groupBy(cls, groupBy):
+        cls.query += f" GROUP BY {groupBy}"
+        return cls
+
+    @classmethod
+    def counts(cls, **kwargs):
+        cls.query = f"SELECT COUNT(*) FROM {cls.table_name}"
+        cls.values = []
+
+        conditions = []
+        values = []
+        for key, value in kwargs.items():
+            conditions.append(f'{key}=?')
+            values.append(value)
+        if len(conditions) > 0:
+            cls.query += f' WHERE {" AND ".join(conditions)}'
+            cls.values += values
+
+        return cls
+
+    @classmethod
+    def fetch(cls):
+        print(cls.query, cls.values)
+        if 'COUNT(*)' not in cls.query:
+            return cls.fmtResult(cls.execute(cls.query, cls.values))
+        else:
+            return cls.execute(cls.query, cls.values)[0][0]
 
     @classmethod
     def execute(cls, query, params=None):
@@ -30,53 +128,6 @@ class DB:
         return result
 
     @classmethod
-    def find(cls, val, key='id', force=False):
-        if not force:
-            query = f'SELECT * FROM {cls.table_name} WHERE {key} = ? AND isdel = 0'
-        else:
-            query = f'SELECT * FROM {cls.table_name} WHERE {key} = ?'
-        data = cls.execute(query, (val,))
-        return cls.fmtResult(data)
-
-    @classmethod
-    def where(cls, **kwargs):
-        query = f'SELECT * FROM {cls.table_name}'
-
-        conditions = []
-        values = []
-        for key, value in kwargs.items():
-            if key == 'limit' or key == 'offset':
-                continue
-            conditions.append(f'{key}=?')
-            values.append(value)
-        if len(conditions) > 0:
-            query += f' WHERE {" AND ".join(conditions)}'
-
-        if 'limit' in kwargs:
-            query += f' LIMIT {kwargs["limit"]}'
-        if 'offset' in kwargs:
-            query += f' OFFSET {kwargs["offset"]}'
-
-        result = cls.execute(query, values)
-        return cls.fmtResult(result)
-
-    def save(self):
-        if 'id' in self.data:
-            query  = f'UPDATE {self.table_name} SET {", ".join([f"{k} = ?" for k in self.data.keys()])} WHERE id = ?'
-            params = list(self.data.values()) + [self.data['id']]
-        else:
-            query  = f'INSERT INTO {self.table_name} ({", ".join(self.data.keys())}) VALUES ({", ".join(["?" for _ in self.data.values()])})'
-            params = list(self.data.values())
-        self.execute(query, params)
-        if 'id' not in self.data:
-            self.data['id'] = self.execute('SELECT last_insert_rowid()')[0][0]
-
-    def delete(self):
-        query = f'DELETE FROM {self.table_name} WHERE id = ?'
-        self.execute(query, (self.data['id'],))
-        self.data = {}
-
-    @classmethod
     def getColumns(cls):
         query = f'PRAGMA table_info({cls.table_name})'
         return cls.execute(query)
@@ -91,47 +142,3 @@ class DB:
                 tmp[colName[i][1]] = row[i]
             result.append(tmp)
         return result
-
-    @classmethod
-    def __init_subclass__(cls, **config):
-        super().__init_subclass__(**config)
-        cls.connect(config.get('db_name', 'Data/config.db'))
-
-    @classmethod
-    def __del_subclass__(cls):
-        cls.close()
-        super().__del_subclass__()
-
-    @classmethod
-    def counts(cls, **kwargs):
-        query = f'SELECT COUNT(*) FROM {cls.table_name}'
-
-        conditions = []
-        values = []
-        for key, value in kwargs.items():
-            if key == 'limit' or key == 'offset':
-                continue
-            conditions.append(f'{key}=?')
-            values.append(value)
-        if len(conditions) > 0:
-            query += f' WHERE {" AND ".join(conditions)}'
-
-        return cls.execute(query)[0][0]
-
-    @classmethod
-    def select(cls, page=1, limit=20, type='normal'):
-        if type == 'normal':
-            query = f'SELECT * FROM {cls.table_name} where isdel = 0 LIMIT ? OFFSET ?'
-        elif type == 'del':
-            query = f'SELECT * FROM {cls.table_name} where isdel = 1 LIMIT ? OFFSET ?'
-        else:
-            query = f'SELECT * FROM {cls.table_name} LIMIT ? OFFSET ?'
-        return cls.execute(query, (limit, (int(page) - 1) * int(limit)))
-
-    @classmethod
-    def selectAll(cls, force=False):
-        if not force:
-            query = f'SELECT * FROM {cls.table_name} where isdel = 0'
-        else:
-            query = f'SELECT * FROM {cls.table_name}'
-        return cls.execute(query)
