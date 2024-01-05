895f39d1b29c16c9672e257c7b702405e570144e
EvilCult <EvilCult@users.noreply.github.com>
2024-01-06 06:24:26 +0800

    添加table一些其他功能框架

--- Projects/WebApp/pages/channel/list.tsx
+++ Projects/WebApp/pages/channel/list.tsx
@@ -17,10 +17,19 @@ import {
   IconButton,
   Menu,
   MenuItem,
-  Avatar,
+  ListItemIcon,
+  ListItemText,
+  InputBase,
+  Divider,
 } from '@mui/material'
 import {
   Tune as TuneIcon,
+  DeleteForever as DeleteForeverIcon,
+  Edit as EditIcon,
+  Speed as SpeedIcon,
+  Menu as MenuIcon,
+  Search as SearchIcon,
+  MoreVert as MoreVertIcon,
 } from '@mui/icons-material'
 import AppHeader from '@/components/appheader'
 import Copyright from '@/components/appfooter'
@@ -32,6 +41,7 @@ const DataTable = () => {
   const [limit, setLimit] = useState(10)
   const [count, setCount] = useState(0)
   const [list, setlist] = useState([])
+  const [update, setUpdate] = useState(false)
   const {loading, data, msg, setMsg} = useChannelList(page + 1, limit)
 
   useEffect(() => {
@@ -40,6 +50,17 @@ const DataTable = () => {
       setCount(data.data.count)
     }
   }, [loading, data, msg])
+
+  useEffect(() => {
+    if (update){
+      const newPage = page
+      setPage(-1)
+      setUpdate(false)
+      setTimeout(() => {
+        setPage(newPage)
+      }, 500)
+    }
+  }, [page, update])
 
 
   const handleChangePage = (event: unknown, newPage: number) => {
@@ -59,18 +80,45 @@ const DataTable = () => {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
+              <TableCell colSpan={6} align='right'>
+                <Paper
+                  component="form"
+                  sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: '100%' }}
+                >
+                  <SearchIcon sx={{ p: '2px' , ml: '4px'}} color="disabled"/>
+                  <InputBase
+                    sx={{ ml: 1, flex: 1 }}
+                    placeholder="Search"
+                    inputProps={{
+                      'aria-label': 'search',
+                      onKeyPress: (event) => {
+                        if (event.key === 'Enter') {
+                          event.preventDefault();
+                        }
+                      }
+                    }}
+                  />
+                  <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+                  <IconButton sx={{ p: '10px' }} aria-label="directions">
+                    <MoreVertIcon />
+                  </IconButton>
+                </Paper>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
               <TableCell>ID</TableCell>
               <TableCell align="left">{lang('page_channel_list_title')}</TableCell>
+              <TableCell align="center">URL</TableCell>
               <TableCell align="right">{lang('page_channel_list_stat')}</TableCell>
               <TableCell align="right">{lang('page_channel_list_ping')}</TableCell>
               <TableCell align="right"></TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
           {
-            loading ?
+            loading && list.length === 0 ?
             <TableRow>
-              <TableCell colSpan={4}>
+              <TableCell colSpan={6}>
                 <Skeleton variant="rectangular" height={100} />
               </TableCell>
             </TableRow>
@@ -82,6 +130,8 @@ const DataTable = () => {
                 title={row.title}
                 alive={row.alive}
                 ping={row.ping}
+                url={row.url}
+                recall={setUpdate}
               />
             ))
           }
@@ -106,16 +156,24 @@ interface TableRowsProps {
   readonly title: string,
   readonly alive: number,
   readonly ping: number,
+  readonly url: string,
+  recall?: any
 }
 const TableRows = (props: TableRowsProps) => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl)
+
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget)
-  };
+  }
   const handleClose = () => {
     setAnchorEl(null)
-  };
+  }
+
+  const handleEdit = () => {
+    handleClose()
+    props.recall(true)
+  }
 
   return (
     <React.Fragment>
@@ -125,7 +183,13 @@ const TableRows = (props: TableRowsProps) => {
         <TableCell component="th" scope="row" width={80}>
           {props.id}
         </TableCell>
-        <TableCell align="left" width={300}>{props.title}</TableCell>
+        <TableCell
+          align="left"
+          sx={{
+            minWidth: 200,
+          }}
+        >{props.title}</TableCell>
+        <TableCell align="center" width={300}>{props.url}</TableCell>
         <TableCell align="right">{
           props.alive === 1 && props.ping !== 0
           ? <Chip size="small" label={lang('channel_stat_alive')} color="success" />
@@ -150,7 +214,7 @@ const TableRows = (props: TableRowsProps) => {
         id="account-menu"
         open={open}
         onClose={handleClose}
-        onClick={handleClose}
+        onClick={handleEdit}
         PaperProps={{
           elevation: 0,
           sx: {
@@ -175,13 +239,31 @@ const TableRows = (props: TableRowsProps) => {
               transform: 'translateY(-50%) rotate(45deg)',
               zIndex: 0,
             },
+            '.MuiMenuItem-root':  {
+              width: 150,
+            }
           },
         }}
         transformOrigin={{ horizontal: 'right', vertical: 'top' }}
         anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
-        <MenuItem onClick={handleClose}>
-            Profile
+        <MenuItem>
+          <ListItemIcon>
+            <EditIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Edit</ListItemText>
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <SpeedIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Ping</ListItemText>
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <DeleteForeverIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Del</ListItemText>
         </MenuItem>
       </Menu>
     </React.Fragment>
@@ -195,6 +277,10 @@ const ChannelList = () => {
       <AppHeader />
       <Box component="main" sx={{ flexGrow: 1, p: 3, display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
         <Toolbar />
+        <Box sx={{ mb: 2}}>
+          <Typography variant="h6" gutterBottom> {lang('menu_channel_list')} </Typography>
+        </Box>
+        <CssBaseline />
         <DataTable />
         <Box sx={{ mt: 'auto' , p: 2}}><Copyright /></Box>
       </Box>
